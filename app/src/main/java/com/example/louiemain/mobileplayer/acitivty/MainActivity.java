cfd74fc9fe81296722e1b4ae48972c1cd0f9a7f3
louiema1n <louielcxy@live.cn>
2018-03-12 17:57:01 +0800

    Initial commit

---
+++ app/src/main/java/com/example/louiemain/mobileplayer/acitivty/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.louiemain.mobileplayer.acitivty;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.louiemain.mobileplayer.R;
+
+/**
+ * @Program: MobilePlayer
+ * @Type: Class
+ * @Description:
+ * @Author: louiemain
+ * @Create: 2018-03-12 17:48
+ **/
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
