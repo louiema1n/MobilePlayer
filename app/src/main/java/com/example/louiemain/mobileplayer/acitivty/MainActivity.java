c109becb36aae96f33612a6c7a2cc3d66c38b587
louiemain <louielcxy@live.cn>
2018-03-12 23:48:56 +0800

    实现aciton bar

--- app/src/main/java/com/example/louiemain/mobileplayer/acitivty/MainActivity.java
+++ app/src/main/java/com/example/louiemain/mobileplayer/acitivty/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.louiemain.mobileplayer.acitivty;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import com.example.louiemain.mobileplayer.R;
 
 /**
@@ -13,10 +15,19 @@ import com.example.louiemain.mobileplayer.R;
  **/
 public class MainActivity extends Activity {
 
+    private RadioGroup rg_bottom_tag;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        
+        // 实例化
+        rg_bottom_tag = (RadioGroup) findViewById(R.id.rg_bottom_tag);
+
+        // 默认选中
+        rg_bottom_tag.check(R.id.rb_video);
+
     }
 }
