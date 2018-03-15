a6b6a1439cf535b5cd26dfb62f1bf7c6207c3152
louiema1n <louielcxy@live.cn>
2018-03-15 17:58:57 +0800

    实现系统视频播放

--- app/src/main/java/com/example/louiemain/mobileplayer/acitivty/SystemVideoPlayer.java
+++ app/src/main/java/com/example/louiemain/mobileplayer/acitivty/SystemVideoPlayer.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
@@ -19,6 +21,7 @@ import com.example.louiemain.mobileplayer.R;
 public class SystemVideoPlayer extends Activity {
 
     private VideoView tv_video_player;
+    private ImageButton ib_start_pause;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,7 @@ public class SystemVideoPlayer extends Activity {
         // 实例化视图
         setContentView(R.layout.activity_system_video_player);
         tv_video_player = (VideoView) findViewById(R.id.tv_video_player);
+        ib_start_pause = (ImageButton) findViewById(R.id.ib_start_pause);
 
         // 准备好的监听
         tv_video_player.setOnPreparedListener(new MediaPlayer.OnPreparedListener(){
@@ -61,5 +65,23 @@ public class SystemVideoPlayer extends Activity {
             // 设置系统控制组件
             tv_video_player.setMediaController(new MediaController(this));
         }
+
+        // 设置点击监听
+        ib_start_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (tv_video_player.isPlaying()) {
+                    // 播放中，设置 暂停
+                    tv_video_player.pause();
+                    // 修改图标为播放
+                    ib_start_pause.setImageDrawable(getResources().getDrawable(R.drawable.btn_play_selector, null));
+                } else {
+                    // 暂停，设置 播放
+                    tv_video_player.start();
+                    // 修改图标为暂停
+                    ib_start_pause.setImageDrawable(getResources().getDrawable(R.drawable.btn_pause_selector, null));
+                }
+            }
+        });
     }
 }
